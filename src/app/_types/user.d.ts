0e8ba69f594138d0e3174d9069dd72a3b045a353
alexia <alexia.souvane@promyze.com>
2020-10-10 10:21:38 +0200

    interface: user

---
+++ src/app/_types/user.d.ts
@@ -0,0 +1,10 @@
+export interface UserI {
+    creationDate: string
+    email: string | null
+    firstName: string | null
+    id: string
+    lastName: string | null
+    modificationDate: string
+    phone: string | null
+    type: string
+}
