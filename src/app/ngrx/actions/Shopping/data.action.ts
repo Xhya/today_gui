acc8395b50dd187ad3eefed7b527fd7432385b9f
alexia <alexia.souvane@promyze.com>
2020-10-17 16:45:45 +0200

    refactoring: remove useless files

--- src/app/ngrx/actions/Shopping/data.action.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { ListOfProductI } from 'src/app/_types/listOfProduct';
-
-export const setListOfProduct = createAction('[Shopping Data Component] setListOfProduct',  props<{listOfListOfProduct: ListOfProductI[]}>());
-export const addListOfProduct = createAction('[Shopping Data Component] addListOfProduct',  props<{listOfProduct: ListOfProductI}>());
-
-export const setCurrentListOfProduct = createAction('[Shopping Data Component] setCurrentListOfProduct', props<{ currentListOfProduct: ListOfProductI }>());
-export const addProductsInCurrentListOfProduct = createAction('[Shopping Data Component] addProductsInCurrentListOfProduct', props<{ product: ProductI }>());
-export const setNameOfCurrentListOfProduct = createAction('[Shopping Data Component] setNameOfCurrentListOfProduct', props<{ name: string }>());
